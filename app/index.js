93af7ad38f19a5081b5ccb1029f543760be93d4e
Fabien JUIF <fabien.juif@zenika.com>
2016-03-16 16:32:44 +0100

    Premiere version gulp

--- app/index.js
+++ app/index.js
@@ -7,29 +7,29 @@ const nickNames = ['fabien.juif']
 // Expect a SLACK_TOKEN environment variable
 const slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
+    console.error('SLACK_TOKEN is required!')
+    process.exit(1)
 }
 
 const controller = Botkit.slackbot()
 const bot = controller.spawn({
-  token: slackToken
+    token: slackToken
 })
 
-bot.startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
+bot.startRTM(function(err, bot, payload) {
+    if (err) {
+        throw new Error('Could not connect to Slack')
+    }
 })
 
 nickNames.each(nickName => {
-  require('./nick')(controller, nickName)
+    require('./nick')(controller, nickName)
 })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.startConversation(message, (err, conversation) => {
-    conversation.say('Tu as des remarques sur moi ?! C\'est ça ?!')
-    setTimeout(() => conversation.say('Bon ok je ne suis pas infaillible, tu peux poster un bogue ici : https://github.com/fabienjuif/codemylife/issues'), 500)
-    setTimeout(() => conversation.say('Au fait ! Je connais les personnes suivantes : ' + nickNames.map(nickName => '@' + nickName)), 500)
-  })
+controller.hears('.*', ['mention'], (bot, message) => {
+    bot.startConversation(message, (err, conversation) => {
+        conversation.say('Tu as des remarques sur moi ?! C\'est ça ?!')
+        setTimeout(() => conversation.say('Bon ok je ne suis pas infaillible, tu peux poster un bogue ici : https://github.com/fabienjuif/codemylife/issues'), 500)
+        setTimeout(() => conversation.say('Au fait ! Je connais les personnes suivantes : ' + nickNames.map(nickName => '@' + nickName)), 500)
+    })
 })
