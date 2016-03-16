f6b4540344a3288782412b84b1233e62e637323f
Fabien JUIF <fabien.juif@zenika.com>
2016-03-16 22:31:33 +0100

    Adding first NArcade interactions

---
+++ app/narcade/narcade.js
@@ -0,0 +1,40 @@
+module.exports = (controller, playersName) => {
+    for (playerName of playersName) {
+        require('./narcadePlayer')(controller, playerName)
+    }
+
+    controller.hears('^narcade help', ['ambient', 'direct_mention', 'direct_message'], (bot, message) => {
+        var channel = message.channel
+
+        bot.reply(message, 'Ha ouai tu veux de l\'aide :)')
+        setTimeout(() => {
+            bot.say({
+                channel: channel,
+                text: 'C\'est bien...'
+            })
+
+            setTimeout(() => {
+                bot.say({
+                    channel: channel,
+                    text: 'Je rigole !! :D'
+                })
+
+                bot.say({
+                    channel: channel,
+                    text: 'Tu devrais demander de l\'aide a une des ces personnes :' + playersName.map(p => ' ' + p)
+                })
+
+                bot.say({
+                    channel: channel,
+                    text: 'Poliment, par exemple comme ça : "Aide moi à détruire le mur ' + playersName[0] + '"'
+                })
+
+                bot.say({
+                    channel: channel,
+                    text: '(La phrase doit commencer par "Aide moi à", puis il doit y avoir un mot clé (par exemple: "détruire") et se terminer par le nom du joueur invoqué)'
+                })
+            }, 2000)
+        }, 1000)
+
+    })
+}
---
+++ app/narcade/narcadePlayer.js
@@ -0,0 +1,59 @@
+const request = require('request')
+
+const actions = [{
+    code: 'destroy',
+    keywords: ['destroy', 'détruire', 'detruire', 'casser']
+}, {
+    code: 'up',
+    keywords: ['up', 'soulever', 'monter']
+}, {
+    code: 'down',
+    keywords: ['down', 'rabaisser', 'descendre']
+}, {
+    code: 'fix',
+    keywords: ['fix', 'réparer', 'reparer', 'rafistoler']
+}]
+
+// This constant is a helper, it can be used to optimize bot reaction time when there is no match, for instance
+const keywords = actions.map(a => a.keywords).flatten()
+
+module.exports = (controller, playerName) => {
+    controller.hears('^[Aa]ide moi [àa] (.*) ' + playerName + '( .*)?', ['ambient', 'direct_mention', 'direct_message'], (bot, message) => {
+        var channel = message.channel
+
+
+
+        // Trying to find a keyword
+        var matches = message.match[1].words().intersect(keywords)
+
+        // No matches, no chocolate
+        if (matches.length === 0) {
+          bot.reply(message, 'Gné ? Tu veux pas laisser ' + playerName + ' tranquille au lieu de raconter n\'importe quoi !?')
+          return
+        }
+        bot.reply(message, 'Je compose son numéro... tututu ')
+
+        // Matches, we can find the action code
+        var code = actions.filter(a => a.keywords.includes(matches[0]))[0].code
+
+        // Message to send to NArcade
+        var message = {
+          playerName: playerName,
+          action: code
+        }
+
+        request.post('http://localhost:4000/actions', {
+                json: true,
+                body: message
+            },
+            (error, response, body) => {
+                if (error) {
+                    bot.say({
+                        channel: channel,
+                        text: JSON.stringify(error)
+                    })
+                }
+            })
+
+    })
+}
